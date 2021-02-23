69fade2a4168093a1bf12732a38e6aec7abadc53
abramenal <pavel.auramenka@gmail.com>
2021-02-23 21:37:53 +0100

    chore: initial app setup

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverageFrom: ['src/**/**.ts', '!src/types/**.ts', '!src/local.ts', '!**/node_modules/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
