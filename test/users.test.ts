7548d8d6051c6a55a7c69c2601578e78b042a351
abramenal <pavel.auramenka@gmail.com>
2021-02-25 17:41:50 +0100

    test: service e2e tests

--- test/users.test.ts
+++ test/users.test.ts
@@ -1,15 +1,24 @@
 import request from 'supertest';
 
+import connection from '../src/db/connection';
 import createApp from '../src/app';
 import createServices from '../src/services';
 
-const logger = console;
+const logger = {
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
 
 const services = createServices({ logger });
 
 const app = createApp({ logger, services });
 
 describe('/users', () => {
+  afterAll(() => {
+    connection.destroy();
+  });
+
   describe('POST /', () => {
     it('throws if "username" is missing', done => {
       request(app)
@@ -24,6 +33,56 @@ describe('/users', () => {
         });
     });
 
-    it.todo('return a new user');
+    it('return a new user', done => {
+      request(app)
+        .post('/users')
+        .send({ username: 'abramenal' })
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          expect(response.body.username).toEqual('abramenal');
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  describe('GET /:userId/history', () => {
+    it('returns user games history', done => {
+      request(app)
+        .get('/users/1b059374-dc5a-4552-86b1-b807a0ac2734/history')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          expect(response.body).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({ value: '1', status: 'completed' }),
+              expect.objectContaining({ value: '2', status: 'completed' }),
+              expect.objectContaining({ value: '3', status: 'completed' }),
+              expect.objectContaining({ value: '4', status: 'completed' }),
+            ]),
+          );
+          done();
+        })
+        .catch(err => done(err));
+    });
+  });
+
+  describe('GET /:username', () => {
+    it('returns user by username', done => {
+      request(app)
+        .get('/users/test')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+          expect(response.body).toEqual(
+            expect.objectContaining({
+              username: 'test',
+            }),
+          );
+          done();
+        })
+        .catch(err => done(err));
+    });
   });
 });
