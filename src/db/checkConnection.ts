2081fbc4817355c31f93b7ce5e0252309090441b
abramenal <pavel.auramenka@gmail.com>
2021-02-27 13:20:51 +0100

    feat: check database connection

---
+++ src/db/checkConnection.ts
@@ -0,0 +1,18 @@
+import retry from 'async-retry';
+import connection from './connection';
+
+import { Logger } from '../logger';
+
+export default async function checkConnection(logger: Logger) {
+  await retry(
+    async () => {
+      await connection.raw('SELECT 1+1 as result');
+    },
+    {
+      retries: 5,
+      onRetry: () => {
+        logger.warn('Cannot establish DB connection, retrying...');
+      },
+    },
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { APP_PORT, NODE_ENV } from './bootstrap';
 
 import './db/connection';
+import checkConnection from './db/checkConnection';
 
 import createLogger from './logger';
 import createApp from './app';
@@ -17,4 +18,12 @@ const app = createApp({ logger, services });
 
 app.listen(APP_PORT, () => {
   logger.info(`Application is started on "${APP_PORT}" port`);
+  checkConnection(logger)
+    .then(() => {
+      logger.info('Database connection is set');
+    })
+    .catch(err => {
+      logger.info('Database connection failed', err);
+      process.exit(1);
+    });
 });
