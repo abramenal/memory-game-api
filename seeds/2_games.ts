78405a4971e739722e6cb8d6d6d81dfef57962b9
abramenal <pavel.auramenka@gmail.com>
2021-02-27 14:41:45 +0100

    feat: update seeds to reflect new db model

--- seeds/2_games.ts
+++ seeds/2_games.ts
@@ -7,7 +7,7 @@ export async function seed(knex: Knex): Promise<void> {
     {
       id: 'a6321738-f691-4adc-968e-a60b42276fd7',
       status: 'completed',
-      sequence: '1,2,3,4',
+      sequence: [1, 2, 3, 4],
       current_turn: 4,
       turns_total: 4,
       user_id: '1b059374-dc5a-4552-86b1-b807a0ac2734',
@@ -15,7 +15,7 @@ export async function seed(knex: Knex): Promise<void> {
     {
       id: 'a2e919c1-882c-4ba7-b9c5-3f4ec58f1fa3',
       status: 'started',
-      sequence: '5,20,1,4',
+      sequence: [5, 20, 1, 4],
       current_turn: 3,
       turns_total: 4,
       user_id: 'ee0e8b34-b2d3-4fba-bb48-d56d14a9aabc',
@@ -23,7 +23,7 @@ export async function seed(knex: Knex): Promise<void> {
     {
       id: '150d3a34-4346-49d0-a71c-7063a178bc50',
       status: 'started',
-      sequence: '10,20,30,40',
+      sequence: [10, 20, 30, 40],
       current_turn: 3,
       turns_total: 4,
       user_id: 'ee0e8b34-b2d3-4fba-bb48-d56d14a9aabc',
@@ -31,7 +31,7 @@ export async function seed(knex: Knex): Promise<void> {
     {
       id: '037d4554-51d4-4004-b6dd-5c772644c9cf',
       status: 'started',
-      sequence: '6,7,8,9',
+      sequence: [6, 7, 8, 9],
       current_turn: 0,
       turns_total: 4,
       user_id: 'ee0e8b34-b2d3-4fba-bb48-d56d14a9aabc',
